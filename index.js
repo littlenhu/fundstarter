72d317e28179fdfdac09b2eae42ffab655cc4663
Ubuntu <ubuntu@ip-172-31-14-200.us-west-2.compute.internal>
2016-09-29 03:00:51 +0000

    make it better

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-console.log(fs.readFileSync('index.html', 'utf8'));
+var fs = require("fs");
+var file_name = "public/index.html";
+var input = fs.readFileSync(file_name, "utf8");
+app.set('port', (process.env.PORT || 8080))
+var port = process.env.PORT || 8080
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at :" + app.get('port'))
+})
