8a2adc5f3d2c46e063e09b225734528bec5d4136
Ubuntu <ubuntu@ip-172-31-14-200.us-west-2.compute.internal>
2016-09-30 01:43:53 +0000

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var http = require("http");
-var fs =require("fs");
-var data = fs.readFileSync('index.html');
-//web server object
-http.createServer(function (request, response) {
-response.writeHead(200, {
-'Content-Type': 'text/html'
-});
-response.write(data);
-response.end();
-}).listen(8080);
+	var http = require("http");
+ 	var fs =require("fs");
+ 	// calling the readFile function
+ 	fs.readFile('index.html',function (err, data)
+ 	{
+
+ 		if(err)
+ 		{
+ 			console.log("File was not read properly.\n");
+ 		}
+       		
+ 		//web server object
+		http.createServer(function (request, response) 
+		{
+ 			response.writeHead(200, {'Content-Type': 'text/html'});
+ 			response.write(data);
+			response.end();
+ 		}).listen(8080);
+	});
